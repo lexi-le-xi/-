90762951976e021123911803ecac3c0ac322672f
lexi-le-xi <184223010+lexi-le-xi@users.noreply.github.com>
2026-08-19 01:08:36 -0700

    Deploy game with GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "-";
+const pagesBasePath = isGitHubPages ? `/${repositoryName}` : "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: pagesBasePath,
+  assetPrefix: pagesBasePath,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
